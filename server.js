2aead1edb5170c390b2b77fb8ecd7ee6365f1d0c
supreet29 <32635530+supreet29@users.noreply.github.com>
2017-12-08 00:19:48 +0530

    Add files via upload
    -added a node module which connects node with github
    -added a npm module name es6-plato which which generate report for cyclomatic complexity of the repository

--- server.js
+++ server.js
@@ -1,40 +1,164 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3002;
-  var multer = require('multer');  //handles form-data used for uploading files
-  var bodyParser = require('body-parser');
-  var ejs = require('ejs');
-  var path = require('path')
+app = express(),
+port = process.env.PORT || 3002;
+//var multer = require('multer');  //handles form-data used for uploading files
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var plato = require('es6-plato') // module to generate reports which includes visual reports stored in the artifacts/plato
+let gulp = require('gulp');
+//var plato = require('plato');
+var path = require('path')
+ app.set('view engine', 'ejs')
 
-  var storage = multer.diskStorage({
-	destination: function(req, file, callback) {
-		callback(null, './uploads')
-	},
-	filename: function(req, file, callback) {
-		console.log(file)
-		callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
-	}
-})
-  app.set('view engine', 'ejs')
+const GitHubApi = require('github')// npm module to link node api to github
 
-app.get('/api/file', function(req, res) {
-	res.render('index')
+
+app.get('/api/file',function(req,res){
+  const github = new GitHubApi({
+    debug: true
+  })
+
+  const customHeaders = {
+    'User-Agent': 'supreet29'
+  }
+
+  github.issues.getForRepo({
+    owner: 'octokit',
+    repo: 'node-github',
+    headers: customHeaders
+  })
+
+  github.repos.getContent({
+    owner: 'octokit',
+    repo: 'node-github',
+    path: ''
+  })
+// token generated from the github account
+  github.authenticate({
+    type: 'oauth',
+    token: 'cac0257a0f37621041af66827758f4fe1a7714d5'
+  })
+
+  github.repos.getAll({
+    'affiliation': 'owner,organization_member'
+  },function(err,res){
+    const repositoryName = []
+    const repositoryUrl = []
+    if(err)
+    console.log(err)
+    else
+    //console.log('******', res.data.length)
+    res.data.forEach(repo => {
+      repositoryName.push(repo.name)
+      repositoryUrl.push(repo.html_url)
+    })
+
+
+    let src = repositoryUrl
+    let outputDir = './artifacts/plato';
+    let complexityRules = {}
+    let lintRules = {
+      'rules': {
+        'indent': [2,'tab'],
+        'quotes': [2,'single'],
+        'semi': [2,'always'],
+        'no-console' : [1],
+        'curly': ['error'],
+        'no-dupe-keys': 2,
+        'func-names': [1, 'always']
+      },
+      'env': {
+        'es6': true
+      },
+      'globals':['require'],
+      'parserOptions' : {
+        'sourceType': 'module',
+        'ecmaFeatures': {
+          'jsx': true,
+          'modules': true
+        }
+      }
+    };
+
+  let platoArgs = {
+    title: 'example',
+    eslint: lintRules,
+    complexity: complexityRules
+  };
+
+  //you can use the reports in the callback.
+  function callback(reports){
+    let overview = plato.getOverviewReport(reports);
+
+    let {
+      total,
+      average
+    } = overview.summary;
+
+    let output = `total
+      ----------------------
+      eslint: ${total.eslint}
+      sloc: ${total.sloc}
+      maintainability: ${total.maintainability}
+      average
+      ----------------------
+      eslint: ${average.eslint}
+      sloc: ${average.sloc}
+      maintainability: ${average.maintainability}`;
+
+    console.log(output);
+  }
+
+
+  //usage is plato.inspect
+  function analysis() {
+    return plato.inspect(src, outputDir, platoArgs);
+  }
+
+  gulp.task('analysis', analysis);
+  console.log("&&&@@@@*******", gulp.task('analysis', analysis))
+    console.log('***** repositoryName',repositoryName)
+    console.log('***** repositoryUrl', repositoryUrl)
+  })
+
+  github.authenticate({
+    type: 'basic',
+    username: 'supreet29',
+    password: 'ZeAl!)(*1'
+  })
+
+  // oauth
+
+
+  // oauth key/secret (to get a token)
+  github.authenticate({
+    type: 'oauth',
+    key: '85cccee1703eeea894aa',
+    secret: '43d16d12696429af5f58be4e5fffe2115c5e3818'
+  })
+
+  // user token
+  github.authenticate({
+    type: 'token',
+    token: 'userToken'
+  })
+
+  // integration (jwt)
+  github.authenticate({
+    type: 'integration',
+    token: 'jwt'
+  })
+
+  // ~/.netrc
+  github.authenticate({
+    type: 'netrc'
+  })
+
+  res.render('display')
 })
 
 app.post('/api/file', function(req, res) {
-	var upload = multer({
-		storage: storage,
-		fileFilter: function(req, file, callback) {
-			var ext = path.extname(file.originalname)
-			if (ext !== '.java' && ext !== '.js' && ext !== '.c' && ext !== '.html'&& ext !== '.csv'&& ext !== '.pdf') {
-				return callback(res.end('Only images are allowed'), null)
-			}
-			callback(null, true)
-		}
-	}).single('userFile')
-	upload(req, res, function(err) {
-		res.end('File is uploaded')
-	})
+
 })
 
 
